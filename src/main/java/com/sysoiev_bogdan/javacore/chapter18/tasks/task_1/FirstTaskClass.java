c0c7c1ae3d19b35c45cad69cf8ad2d20e679c87b
Bada1208 <sysoievbogdan@gmail.com>
2020-06-17 14:58:01 +0200

    done task1

---
+++ src/main/java/com/sysoiev_bogdan/javacore/chapter18/tasks/task_1/FirstTaskClass.java
@@ -0,0 +1,37 @@
+package com.sysoiev_bogdan.javacore.chapter18.tasks.task_1;
+
+
+import java.util.Arrays;
+
+/**
+ * Задание 1
+ * <p>
+ * Дан массив целых чисел. Необходимо реализовать метод, который возвращает индексы массива,
+ * значения которых в сумме дают число переданное в массив.
+ * <p>
+ * Предполагается, что массив содержит только уникальные числа и всегда имеет искомые числа.
+ * Один и тот же элемент не может быть использован дважды.
+ * <p>
+ * array = [3, 8, 15, 17], Number = 23
+ * result = [1,2]
+ * <p>
+ * Т.е. индексы 1 и 2 (значение 8 и 15) в сумме дают искомое число 23
+ */
+public class FirstTaskClass {
+    static void indexSum(int array[], int indexSum) {
+        System.out.println("array = " + Arrays.toString(array) + ", Number = " + indexSum);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i != j && array[i] + array[j] == indexSum) {
+                    System.out.println("result = [" + i + "," + j + "]");
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 8};
+        indexSum(arr, 13);
+    }
+}
