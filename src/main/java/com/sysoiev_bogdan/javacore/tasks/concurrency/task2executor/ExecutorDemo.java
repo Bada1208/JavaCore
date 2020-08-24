42e019d88c2ff04d072c9a1896a3181b0ea222d8
Bada1208 <sysoievbogdan@gmail.com>
2020-08-24 13:28:49 +0200

    add executor service

---
+++ src/main/java/com/sysoiev_bogdan/javacore/tasks/concurrency/task2executor/ExecutorDemo.java
@@ -0,0 +1,75 @@
+package com.sysoiev_bogdan.javacore.tasks.concurrency.task2executor;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.IntConsumer;
+
+class ExecutorDemo {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz(20);
+        ExecutorService executorService = Executors.newScheduledThreadPool(4);
+        //first thread
+        Runnable runnableFizz = () -> {
+            Runnable fizz = () -> {
+            };
+            try {
+                fizzBuzz.fizz(fizz);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Future future1 = executorService.submit(runnableFizz);
+        //second thread
+        Runnable runnableBuzz = () -> {
+            Runnable buzz = () -> {
+            };
+            try {
+                fizzBuzz.buzz(buzz);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Future future2 = executorService.submit(runnableBuzz);
+        //third thread
+        Runnable runnableFizzBuzz = () -> {
+            Runnable fizzBuzzRun = () -> {
+            };
+            try {
+                fizzBuzz.fizzbuzz(fizzBuzzRun);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Future future3 = executorService.submit(runnableFizzBuzz);
+        //forth thread
+        Runnable runnablePrint = () -> {
+            System.out.println();
+            IntConsumer consumer = null;
+            for (int i = 1; i <= fizzBuzz.getN(); i++) {
+                consumer = a -> System.out.print(a + " ");
+                consumer.accept(i);
+            }
+            try {
+                fizzBuzz.number(consumer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Future future4 = executorService.submit(runnablePrint);
+
+        try {
+            future1.get();
+            future2.get();
+            future3.get();
+            future4.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+}
+
---
+++ src/main/java/com/sysoiev_bogdan/javacore/tasks/concurrency/task2executor/FizzBuzz.java
@@ -0,0 +1,72 @@
+package com.sysoiev_bogdan.javacore.tasks.concurrency.task2executor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntConsumer;
+
+class FizzBuzz {
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        printFizz = () -> {
+            System.out.println();
+            List<Object> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 != 0) list.add(i);
+                else if (i % 3 == 0) list.add("fizz");
+            }
+            System.out.println();
+            for (Object o : list) {
+                System.out.print(o.toString() + " ");
+            }
+        };
+        printFizz.run();
+    }
+
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        printBuzz = () -> {
+            System.out.println();
+            List<Object> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (i % 5 != 0) list.add(i);
+                else if (i % 5 == 0) list.add("buzz");
+            }
+            for (Object o : list) {
+                System.out.print(o.toString() + " ");
+            }
+        };
+        printBuzz.run();
+    }
+
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        printFizzBuzz = () -> {
+            System.out.println();
+            List<Object> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 != 0 && i % 5 != 0) list.add(i);
+                else if (i % 3 == 0) list.add("fizz");
+                else if (i % 5 == 0) list.add("buzz");
+            }
+            for (Object o : list) {
+                System.out.print(o.toString() + " ");
+            }
+        };
+        printFizzBuzz.run();
+    }
+
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        printNumber = (n) -> {
+        };
+
+    }
+}
+
