859951af37bd3be4a4df9d4a5f70e67314388823
Bada1208 <sysoievbogdan@gmail.com>
2020-09-06 14:45:37 +0200

    add palindrom

---
+++ src/main/java/com/sysoiev_bogdan/javacore/tasks/palindrom/PalindromDemo.java
@@ -0,0 +1,18 @@
+package com.sysoiev_bogdan.javacore.tasks.palindrom;
+
+public class PalindromDemo {
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("123321"));
+        System.out.println(isPalindrom("1221"));
+        System.out.println(isPalindrom("123456"));
+        System.out.println(isPalindrom("12345678910"));
+        System.out.println(isPalindrom("123456654321"));
+    }
+
+    public static boolean isPalindrom(String s) {
+        boolean isPal = false;
+        String reverse = new StringBuffer(s).reverse().toString();
+        if (s.equals(reverse)) isPal = true;
+        return isPal;
+    }
+}
