7e4f7b79c722074bbef392a98bbddb6dbcf863f8
Bada1208 <sysoievbogdan@gmail.com>
2021-01-10 20:32:14 +0100

    add description

--- src/main/java/com/sysoiev_bogdan/javacore/algorithms/fibonacci/FibonacciDemo.java
+++ src/main/java/com/sysoiev_bogdan/javacore/algorithms/fibonacci/FibonacciDemo.java
@@ -1,15 +1,12 @@
 package com.sysoiev_bogdan.javacore.algorithms.fibonacci;
 
 public class FibonacciDemo {
-
     public static void main(String[] args) {
 //первые два числа равны либо 1 и 1, либо 0 и 1, а каждое последующее число равно сумме двух предыдущих чисел
         System.out.println(fibonacci(5));//8=0,1,1,2,3,5
-
     }
 
     public static int fibonacci(int n) {
         return n <= 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
     }
-
 }
